0f969fb588d54cbe50e62e7e73c352ac14d95021
zblxst <zblxst@yopmail.com>
2024-02-13 11:01:37 +0100

    Clean results

--- TD2/line_mat.py
+++ TD2/line_mat.py
@@ -15,15 +15,15 @@ dim = 120
 # Initialisation de la matrice
 part = ((dim*rank) // nbp, (dim*(rank+1)) // nbp)
 A = np.array([[(i+j) % dim+1. for i in range(dim)] for j in range(part[0], part[1])])
-print(f"A = {A}")
+# print(f"A = {A}")
 
 # Initialisation du vecteur u
 u = np.array([i+1. for i in range(dim)])
-print(f"u = {u}")
+# print(f"u = {u}")
 
 # Produit matrice-vecteur
 v = A.dot(u)
-print(f"v = {v}")
+# print(f"v = {v}")
 
 data = np.concatenate(globCom.allgather(v), axis=0)
 
